742d4b929ab5c4f8628798690bde4f8eebdbe9db
Adegoke Moses Oluwaseun <genius.splendid@gmail.com>
2021-05-04 23:20:37 +0100

    CRUD ASSIGMENT

---
+++ config/blog/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
+from django.shortcuts import  render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+
+
+
+
+# Create your views here.
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
+
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("main:homepage")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm
+	return render (request=request, template_name="main/register.html", context={"register_form":form})
